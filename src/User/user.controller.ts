ebd50a1f62ff69d2a6ffad71ed80595d89c967b2
Arun Chaudhary <arunkhw1998@gmail.com>
2023-08-22 13:58:05 +0530

    ci: working on @param(), DTO and Date() format

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './DTO/create_user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): string {
-    return this.userService.getHello();
+  getData() {
+    return this.userService.findAll();
+  }
+
+  // how to take one paramter in the api
+  @Get(':id')
+  // getHello(@Param('id') id: number) {
+  getHello(@Param('id') id: string) {
+    console.log(`we are taking this parameter from the api -->  ${id}`);
+    // By default the id's datatype is string , so we need to typeCast to ""number""
+    // return this.userService.findOneById(Number(id));
+    return this.userService.findOneById(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
 }
