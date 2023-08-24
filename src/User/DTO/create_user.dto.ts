41a55295d9ef6e4f25821663190c37e0a92de15b
Arun Chaudhary <arunkhw1998@gmail.com>
2023-08-24 17:24:40 +0530

    ci: learning about the swagger and we can explore more

--- src/User/DTO/create_user.dto.ts
+++ src/User/DTO/create_user.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  // This is for the swagger ui, to show the id datatype in the swagger ui
+  @ApiProperty({ required: false })
+  // If the parameter is optinal then we need to make it in this way, bcz swagger don't know about it
   id?: string;
+
+  @ApiProperty()
   name: string;
 }
--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './DTO/create_user.dto';
-
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
+@ApiTags('user')
+// This is for seperarting the api's in the swagger ui
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // This will tell us that the api will return the User type in Array format
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getData() {
     return this.userService.findAll();
   }
 
+  // This will also return the User type as above not in array format
+  @ApiOkResponse({ type: User })
   // how to take one paramter in the api
   @Get(':id')
   // getHello(@Param('id') id: number) {
@@ -21,8 +28,11 @@ export class UserController {
     return this.userService.findOneById(id);
   }
 
+  // Here we are telling the swagger that this api will return the User type
+  @ApiCreatedResponse({ type: User })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
 }
+// ****** WE CAN ALSO EXPLORE ABOUT SWAGGER DECORATORS USIGN DOCUMENTATION ****** //
