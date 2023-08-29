b1ae030ccbadbdec734b7b99f7db3ac0936c2a6a
Arun Chaudhary <arunkhw1998@gmail.com>
2023-08-29 17:06:58 +0530

    set-up health module for the app

---
+++ src/health/health.controller.ts
@@ -0,0 +1,20 @@
+import { Controller } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  async getApiHealth() {
+    return 'available';
+  }
+}
